f9ce485605c8eee219c3328b77bd880d9b2df9e7
Ibrahim Itani <31937332+ibrahimitani0@users.noreply.github.com>
2019-11-04 13:10:39 -0500

    Update dbinfo.php

--- dbinfo.php
+++ dbinfo.php
@@ -1,8 +1,8 @@
 <?php
 
 $user = 'root';
-$pass = '12345678';
-$db = 'PokemonPokedex';
-$host= 'pokemonpokedex.c2mgephvrle7.us-east-1.rds.amazonaws.com';
+$pass = '';
+$db = 'PokeLookUp';
+$host= 'localhost';
 
 ?>
