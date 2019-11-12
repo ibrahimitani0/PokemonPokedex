a775c13b0fb5000aaa531d3ffdfe7daf500f0f5a
Ibrahim Itani <31937332+ibrahimitani0@users.noreply.github.com>
2019-11-11 20:10:09 -0500

    Update dbinfo.php

--- dbinfo.php
+++ dbinfo.php
@@ -1,9 +1,9 @@
 <?php
 
-$user = 'root';
-$pass = '';
+$user = 'admin';
+$pass = 'admin12345678';
 $db = 'PokemonPokedex';
-$host= 'localhost';
+$host= 'mydbpokemon.c2mgephvrle7.us-east-1.rds.amazonaws.com';
 
 $mysqli = new mysqli($host, $user, $pass = '', $db);
 
